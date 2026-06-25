fcd1cbb48521df11a194169ebec5f2cca8d7bf1e
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-06-25 17:16:44 +0200

    change to model to gemini 2.5 Flash for better performnce

--- software_bug_assistant/agent.py
+++ software_bug_assistant/agent.py
@@ -1,7 +1,9 @@
 from google.adk.agents.llm_agent import Agent
 
+MODEL = 'gemini-2.5-flash'
+
 root_agent = Agent(
-    model='gemini-3.5-flash',
+    model=MODEL,
     name='root_agent',
     description='A helpful assistant for user questions.',
     instruction='Answer user questions to the best of your knowledge',
