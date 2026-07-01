bba0a970520e36c1b7af04bba321c5340b342893
Tafadzwa Sibanda <sibusitf@gmail.com>
2026-07-01 14:04:17 +0200

    created GitHub MCP Toolset

--- software_bug_assistant/tools/__init__.py
+++ software_bug_assistant/tools/__init__.py
@@ -1,3 +1,3 @@
 from .file_tools import write_text, read_text, list_directory
 from .cmd_tools import run_command
-from .misc_tools import search_tool
+from .misc_tools import search_tool, github_mcp_tools
--- software_bug_assistant/tools/misc_tools.py
+++ software_bug_assistant/tools/misc_tools.py
@@ -1,5 +1,7 @@
 from google.adk.agents.llm_agent import LlmAgent
 from google.adk.tools import google_search, AgentTool
+from google.adk.tools.mcp_tool import McpToolset, StreamableHTTPConnectionParams
+import os
 
 search_agent = LlmAgent(
     name='search_agent',
@@ -7,6 +9,16 @@ search_agent = LlmAgent(
     description="A search agent",
     instruction="You are an expert at Google Search.. Your job is to perform a search to solve the problem",
     tools=[google_search]
+)
+
+github_mcp_tools = McpToolset(
+    connection_params=StreamableHTTPConnectionParams(
+        url='https://api.githubcopilot.com/mcp',
+        headers={
+            "Authorization": f"Bearer {os.getenv('GITHUB_ACCESS_TOKEN')}",
+            "X-MCP-Readonly": "True"
+        }
+    )
 )
 
 search_tool = AgentTool(search_agent)
